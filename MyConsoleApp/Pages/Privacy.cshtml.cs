b25666b2e0bb0a0d8bd2e8584431651d47cc1403
oh <xohhwx@gmail.com>
2025-07-21 17:12:40 +0900

    added Header and Footer

---
+++ MyConsoleApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyConsoleApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Privacy 페이지 로직
+        }
+    }
+}
