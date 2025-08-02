f911d6de38aa0de588652137a843a64e8d30cf78
oh <xohhwx@gmail.com>
2025-08-02 20:14:41 +0900

    신기능 추가: 지도 서비스 페이지 구현
    - 구글 지도, 네이버 지도, 카카오맵 통합 서비스 페이지 생성
    - 메인 페이지에 지도 서비스 섹션 추가 (메모와 시간 기능 사이에 배치)
    - 빠른 장소 검색 기능 구현
    - 현재 위치 찾기 기능 추가
    - 반응형 디자인으로 모바일 친화적 구성
    - 각 지도 서비스별 특징과 활용 팁 제공

---
+++ Pages/Map.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyConsoleApp.Pages
+{
+    public class MapModel : PageModel
+    {
+        public void OnGet()
+        {
+            // 지도 페이지 로드 시 실행될 로직
+            // 현재는 특별한 로직이 필요하지 않음
+        }
+    }
+}
